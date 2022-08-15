dcad90750325e1452f2332271dea9853bddabd44
Armaan Ahmad <ahmad.arman.deos@gmail.com>
2022-08-15 12:42:57 +0530

    login fixed

--- controller/authController.js
+++ controller/authController.js
@@ -5,8 +5,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const config = require('../config');
 const User = require('../model/userModel');
-const { userInfo } = require('os');
-const { response } = require('express');
+
 
 
 router.use(bodyParser.urlencoded({extended:true}))
@@ -20,11 +19,10 @@ router.get('/users',(req,res) => {
     })
 })
 
-
-// Register User
-router.post('/register', (req, res) => {
-    // Encrypt password
-    let hashPassword = bcrypt.hashSync(req.body.password,8);
+//register User
+router.post('/register',(req,res) => {
+    //encrypt Password
+    let hashPassword = bcrypt.hashSync(req.body.password,8)
     User.create({
         name:req.body.name,
         email:req.body.email,
@@ -33,36 +31,34 @@ router.post('/register', (req, res) => {
         role:req.body.role?req.body.role:'User'
     },(err,data) => {
         if(err) return res.status(500).send('Error While Register');
-        res.status(200).send('Registration Successful');
+        res.status(200).send('Registeration Succesful')
     })
 })
 
-// Login Users
-router.post('/login', (req,res) => {
-    User.findOne({email:req.body.email}, (err,user) => {
-        if(err) return res.send({auth:false,token:'Error While Login'})
-        if(!user) return res.send({auth:false,token:'No User Found Register First'})
-
-        else{
-            const passIsValid = bcrypt.compareSync(req.body.password,user.password)
-            if(!passIsValid) return res.send({auth:false,token:'Invalid Password'})
-
-            // in case email and password is correct generate token
-            let token = jwt.sign({id:user._id},config.secret,{expiresIn:86400})
-            return res.send({auth:false,token:token})
+//Login Users
+router.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) return res.send({ auth: false, token: "Error while connection" });
+        if (!user) return res.send({ auth: false, token: "No user is found Register first" });
+        else {
+            const passIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if (!passIsValid)
+                return res.send({ auth: false, token: "Invalid password" });
+            // password correct 
+            let token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 86400 })// 24 hours
+            return res.send({ auth: true, token: token });
         }
     })
 })
 
-
-// User information
-router.get('/userInfo', (req, res) => {
+//userinfo
+router.get('/userInfo',(req,res) => {
     let token = req.headers['x-access-token'];
-    if(!token) res.send({auth:false,token:'No Token Provided'});
+    if(!token) res.send({auth:false,token:'No Token Provided'})
     // jwt verify token
     jwt.verify(token,config.secret,(err,user) => {
-        if(err) return res.send({auth:false,token:'Error Verifying Token'})
-        User.findById(user.id, (err, result) => {
+        if(err) return res.send({auth:false,token:'Invalid Token'})
+        User.findById(user.id,(err,result) => {
             res.send(result)
         })
     })
--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const cors = require('cors');
 app.use(cors());
 
 const AuthController = require('./controller/authController');
+
 app.get("/", (req, res) => {
     res.send("Express server is running");
   });
-app.use('/api/auth',AuthController); // inside this authcontroller there are multiple routes.
+app.use('/api/auth',AuthController); //  inside this authcontroller there are multiple routes.
 
 app.listen(port,() => {
     console.log(`Listening on Port https://localhost:${port}`);
